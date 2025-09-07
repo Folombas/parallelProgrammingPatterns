92f27d8cddba7206028218c81a72ff81e97a2c53
Folombas <folombas@yandex.ru>
2025-09-07 20:56:14 +0300

    Изучаем на практике паттерны параллельного программирования

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	ch := make(chan int)
+
+	go func() {
+		ch<-1
+		ch<-2
+		ch<-3
+		ch<-4
+		close(ch)
+	}()
+
+	for v := range ch {
+		fmt.Println(v)
+	}
+
+	fmt.Println("Программы завершена")
+
+	
+}
