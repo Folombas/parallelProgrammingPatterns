73ac4cd3838fd6c43e1dd85a803930951df867cd
Folombas <folombas@yandex.ru>
2025-09-08 22:20:40 +0300

    Изучение паттернов параллельного программирования

--- main2.go
+++ main2.go
@@ -9,20 +9,25 @@ func main() {
 
 	ch := make(chan int)
 	doneCh := make(chan struct{})
+
 	
 
 	
 	go func() {
-		
+		defer func() {
+			doneCh<- struct{}{}
+		}()
 		ch <- 1
 		ch <- 3
 		ch <- 5
 		ch <- 7
-	}()
+		}()
 
 	
 	go func() {
-		
+		defer func() {
+			doneCh<- struct{}{}
+		}()
 		ch <- 2
 		ch <- 4
 		ch <- 6
@@ -30,8 +35,9 @@ func main() {
 	}()
 
 	go func() {
-		
-		<-doneCh
+		for i := 0; i < 2; i++ {
+			<-doneCh
+		}
 		close(ch)
 	}()
 
